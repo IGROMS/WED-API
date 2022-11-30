b0ffe338af5dd6fd6e908c10fa0ae68bdc793f66
Ignacio Romaní <ignacioromani@gmail.com>
2022-11-30 17:49:05 +0100

    authentication settings

---
+++ controllers/auth.controller.js
@@ -0,0 +1,37 @@
+const createError = require("http-errors")
+const jwt = require("jsonwebtoken")
+const User = require("../models/User.model")
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body
+  const LoginError = createError(401, "Email or Password are not valid")
+
+  if(!email || !password) {
+    next(LoginError)
+  }else {
+    User.findOne({ email })
+    .then(user => {
+      if(!user) {
+        next(LoginError)
+      }else {
+        user.checkPassword(password)
+          .then(result => {
+            if(!result) {
+            	next(LoginError)
+            }else {
+              const token = jwt.sign(
+                {
+                	id: user.id
+                },
+                process.env.JWT_SECRET,
+                {
+                  expiresIn: "7d"
+                }
+              )
+              res.json({ accessToken: token });
+            }
+          })
+      }
+    })
+  }
+}
