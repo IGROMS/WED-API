b76ade29c791982c58ed644bf656f73cf79e76a3
Eduard Tudorache <edy_edytza2003@yahoo.com>
2022-11-30 17:49:04 +0100

    work

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
+const authMiddleware = require('../middlewares/auth.middleware')
+const userController = require('../controllers/users.controller')
 
 
 router.get('/', (req, res, next) => res.json({ok: true}))
+
+//USER
+
+router.get('/users/profile', authMiddleware.isAuthenticated, userController.getCurrentUser)
 
 module.exports = router;
---
+++ controllers/users.controller.js
@@ -0,0 +1,14 @@
+const createError = require('http-errors')
+const User = require('../models/User.model')
+
+module.exports.getCurrentUser = (req, res, next) => {
+    User.findById(req.currentUser)
+      .then(user => {
+        if(!user) {
+            next(createError(404, "User not found!"))
+        }else {
+            res.json(user)
+        }
+      })
+      .catch(next)
+}
