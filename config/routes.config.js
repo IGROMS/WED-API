5e630b70317ded91aed1dbdcc207f5ae2c5882f1
Ignacio Romaní <ignacioromani@gmail.com>
2022-11-30 18:14:16 +0100

    login created

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const authMiddleware = require('../middlewares/auth.middleware')
 const userController = require('../controllers/users.controller')
+const authController = require('../controllers/auth.controller')
 
 
 router.get('/', (req, res, next) => res.json({ok: true}))
+
+//AUTH
+
+router.post('/login', authController.login)
+//router.post('/register', authController.register)
 
 //USER
 
